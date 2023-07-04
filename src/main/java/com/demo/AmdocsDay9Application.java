f4fc39cb408887cd33b915f19a4be6aee478d212
dhrutithakrar <dhrutithakrar2001@gmail.com>
2023-07-04 11:57:21 +0530

    first commit

---
+++ src/main/java/com/demo/AmdocsDay9Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmdocsDay9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmdocsDay9Application.class, args);
+	}
+
+}
